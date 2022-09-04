dc6a07ffe931605518546226db7c6ee21f48c5d7
Vector1331 <tpgus4796@gmail.com>
2022-09-04 20:57:44 +0900

    Squashed commit of the following:
    commit 2319fd35df1d58b832b8d36afa176ef6a2a0b8bb
    Merge: 8dbc99b a1b944b
    Author: rosesua318 <71651038+rosesua318@users.noreply.github.com>
    Date:   Sun Sep 4 20:52:03 2022 +0900
    
        Merge pull request #9 from Chaebbi/sua-swagger-01
    
        [Feat] Swagger 적용 (1-1 ~ 5~1)
    
    commit a1b944b501b3dde9c7dc75b07e6e90ae0715f916
    Author: rosesua318 <rosesua@duksung.ac.kr>
    Date:   Sun Sep 4 20:46:27 2022 +0900
    
        [Feat] Swagger 적용 위한 Dto 변경 & SwaggerConfig jwt 설정 변경 (김민서)
    
    commit 20b589ef6ef165d96e958437e6d4c16c0ab3f20e
    Author: rosesua318 <rosesua@duksung.ac.kr>
    Date:   Sun Sep 4 20:45:52 2022 +0900
    
        [Feat] 사용자 관련 API Swagger 적용 (김민서)
    
    commit 63690579d8c3334ea57882f6f303775ceca12baf
    Author: rosesua318 <rosesua@duksung.ac.kr>
    Date:   Sun Sep 4 20:43:53 2022 +0900
    
        [Feat] 식단 관련 API Swaggeer 적용 (김민서)
    
    commit 8d3b817fc4dc54dea520962ddbe358d5b1269fdb
    Author: rosesua318 <rosesua@duksung.ac.kr>
    Date:   Sun Sep 4 20:43:34 2022 +0900
    
        [Feat] 음식 관련 API Swaggeer 적용 (김민서)
    
    commit 49a6690e0079cdf9830083d5a19f9827fdaa355a
    Author: rosesua318 <rosesua@duksung.ac.kr>
    Date:   Sun Sep 4 20:43:07 2022 +0900
    
        [Feat] 식단 분석 Swagger 적용 (김민서)

--- src/main/java/com/ae/chaebbiSpring/dto/request/DateRecordRequestDto.java
+++ src/main/java/com/ae/chaebbiSpring/dto/request/DateRecordRequestDto.java
@@ -1,11 +1,16 @@
 package com.ae.chaebbiSpring.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
+@Schema(description = "식단 조회 request")
 @Data
 public class DateRecordRequestDto {
+    @Pattern(regexp = "\\d{4}.(0[1-9]|1[012]).(0[1-9]|[12][0-9]|3[01]).")
+    @Schema(description = "조회하고 싶은 날짜", example = "2022.09.04.", nullable = false, type = "String")
     @NotNull
     private String date;
 }
--- src/main/java/com/ae/chaebbiSpring/dto/response/RecordsDto.java
+++ src/main/java/com/ae/chaebbiSpring/dto/response/RecordsDto.java
@@ -1,13 +1,18 @@
 package com.ae.chaebbiSpring.dto.response;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
+@Schema(description = "하루 식단 기록")
 @Data
 @AllArgsConstructor
 public class RecordsDto {
+    @Schema(description = "아침, 점심, 저녁 구분", example = "0", nullable = true, type = "int")
     private int meal; // 아침, 점심, 저녁 구분
+    @Schema(description = "매끼니에 섭취한 칼로리 총량", example = "114", nullable = true, type = "int")
     private int mCal; // 한끼니의 총 칼로리
+    @Schema(description = "매끼니에 해당하는 식단 기록들", nullable = true)
     private List<DateRecordDto> record; // 한끼니의 총 식단
 }
