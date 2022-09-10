25607a1bf0799b8df5cb932792f5ec9500e8ac58
rosesua318 <rosesua@duksung.ac.kr>
2022-09-10 11:30:21 +0900

    [Fix] UserUpdateRequestDto 형타입 오류 수정 (김민서)

--- src/main/java/com/ae/chaebbiSpring/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/ae/chaebbiSpring/dto/request/UserUpdateRequestDto.java
@@ -10,12 +10,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateRequestDto {
-    @Schema(name = "age", description = "사용자 수정 나이", nullable = false, example = "26", type = "int")
+    @Schema(name = "age", description = "사용자 수정 나이", nullable = false, example = "26", type = "String")
     private String age;
     @Schema(name = "height", description = "사용자 수정 키", nullable = false, example = "178", type = "String")
     private String height;
     @Schema(name = "weight", description = "사용자 수정 몸무게", nullable = false, example = "78", type = "String")
     private String weight;
-    @Schema(name = "activity", description = "사용자 수정 활동점수", nullable = false, example = "40", type = "int")
+    @Schema(name = "activity", description = "사용자 수정 활동점수", nullable = false, example = "40", type = "String")
     private String activity;
 }
