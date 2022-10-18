50d0739643ebc414148e72ae7cc5a66df6266865
Vector1331 <tpgus4796@gmail.com>
2022-10-18 20:54:33 +0900

    [Refactor] 기존 식단분석 코드 service 로 이동

--- src/main/java/com/ae/chaebbiSpring/service/AnalysisService.java
+++ src/main/java/com/ae/chaebbiSpring/service/AnalysisService.java
@@ -1,11 +1,14 @@
 package com.ae.chaebbiSpring.service;
 
 import com.ae.chaebbiSpring.dto.DateAnalysisDto;
+import com.ae.chaebbiSpring.dto.response.AnalysisDto;
+import com.ae.chaebbiSpring.dto.response.AnalysisResponseDto;
 import com.ae.chaebbiSpring.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,5 +19,35 @@ public class AnalysisService {
 
     public List<DateAnalysisDto> findRecords(Long id) {
         return recordRepository.analysisDate(id);
+    }
+    public AnalysisResponseDto weekAnalysis(List<DateAnalysisDto> weekRecords) {
+        int status = 0;
+        int ratioCarb, ratioPro, ratioFat, totalCarb, totalPro, totalFat;
+        ratioCarb = ratioPro = ratioFat = totalCarb = totalPro = totalFat = 0;
+        //받아온 기록이 7개일 경우 : 정상로직 : status = 1
+        if(weekRecords.size() == 7) {
+            // 식단에 문제가 있는지 확인
+
+            status = 1;
+            List<AnalysisDto> collect = new ArrayList<>();
+
+            for(DateAnalysisDto dateAnalysisDto : weekRecords) {
+                totalCarb += dateAnalysisDto.getSumCarb();
+                totalPro += dateAnalysisDto.getSumPro();
+                totalFat += dateAnalysisDto.getSumFat();
+                collect.add(new AnalysisDto(dateAnalysisDto.getDate(), dateAnalysisDto.getSumCal().intValue()));
+            }
+            int sum = totalCarb + totalPro + totalFat;
+            ratioCarb = totalCarb * 100 / sum;
+            ratioPro = totalPro * 100 / sum;
+            ratioFat = totalFat * 100 / sum;
+            return new AnalysisResponseDto(status, ratioCarb, ratioPro, ratioFat , totalCarb, totalPro, totalFat, collect);
+        }
+        //비정상 로직 status = 0
+        else { return new AnalysisResponseDto(status,ratioCarb, ratioPro, ratioFat , totalCarb, totalPro, totalFat, null);}
+
+
+
+
     }
 }
